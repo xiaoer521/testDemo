f0c8ca15ab33ab7455da6f3b91da486479d77dcd
guohaiping <guohaiping@zuoyebang.com>
2017-10-24 16:02:18 +0800

    Modify:去掉 测试cherry-pick log

--- app/src/main/java/sample/netroid/vincestyling/com/commondemo/MainActivity.java
+++ app/src/main/java/sample/netroid/vincestyling/com/commondemo/MainActivity.java
@@ -29,7 +29,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initView();
-        Log.i("ghppp","测试cherry-pick");
     }
 
     private void initView() {
